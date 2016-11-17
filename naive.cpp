ca91d5caa9a2c6d1df8bd7f9be0247f5573976bb
Ziad Giliana <ZiadGiliana@Ziads-MacBook-Pro.local>
2016-11-17 18:54:55 +0100

    file added

---
+++ naive.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <gmpxx.h>
+using namespace std;
+int main(){
+	mpz_class a, b, c;
+
+  a = 1234;
+  b = "-5678";
+  c = a+b;
+  cout << "sum is " << c << "\n";
+  cout << "absolute value is " << abs(c) << "\n";
+
+
+	return 0;
+}
