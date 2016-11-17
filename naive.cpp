8659906e5b9ba7ee03832fa7fa35ed59bb56655c
Ziad Giliana <ZiadGiliana@Ziads-MacBook-Pro.local>
2016-11-17 23:17:04 +0100

    fixed mpz

--- naive.cpp
+++ naive.cpp
@@ -2,18 +2,44 @@
 #include <stdio.h>
 #include <math.h>
 #include <gmpxx.h>
+#include <vector>
 using namespace std;
+
+void trialDivision(mpz_class &n, vector<mpz_class> &primeFactors){
+	mpz_class rootN;
+	n.get_mpz_t();
+	rootN.get_mpz_t();
+	rootN = sqrt(n);
+	int i = 2;
+	while(i < rootN){
+			if(n % i == 0){
+				cout << i << endl;
+				n = n/i;
+			}else{
+				i++;
+			}
+		}
+		if(n > 1){
+			cout << n << endl;
+		}
+}
+
 int main(){
-	mpz_class a, b, c;
-
-  a = 1234;
-  b = "-5678";
-  c = a+b;
-  cout << "sum is " << c << "\n";
-  cout << "absolute value is " << abs(c) << "\n";
-
-  cout << "just testing in visual studi" << endl;
-  kkrwlke
-
+	vector<mpz_class> primeFactors;
+	vector<mpz_class> numberInput;
+	mpz_class n;
+	mpz_class max = pow(10,16);
+	while(cin >> n){
+		if(n < 2){
+			cout << n << endl;
+		}else if(mpz_cmp(n.get_mpz_t(),max.get_mpz_t()) >= 0){
+			cout << "fail" << endl;
+		}else{
+			mpz_class tmp = n;
+			//primeFactors.erase(primeFactors.begin(), primeFactors.end());
+			trialDivision(tmp, primeFactors);
+		}
+		cout << endl;
+	}
 	return 0;
 }
