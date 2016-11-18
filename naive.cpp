01da4bac4920a9d50675b15c7b06356323a34798
Ziad Giliana <ZiadGiliana@n154-p255.kthopen.kth.se>
2016-11-18 13:19:07 +0100

    fixed primes

--- naive.cpp
+++ naive.cpp
@@ -11,20 +11,20 @@ void getPrimes(vector<mpz_class> &primes, mpz_class &n){
 	n.get_mpz_t();
 	rootN.get_mpz_t();
 	rootN = sqrt(n);
+		cout << "root is " << rootN << endl;
 	for(mpz_class i = 0; i <= rootN; i++){
-		//cout << "root is " << rootN << endl;
+
 		if(mpz_probab_prime_p(i.get_mpz_t(), 50) > 0){
 			primes.push_back(i);
-	//		cout << "pushed " << i << endl;
+			cout << "pushed " << i << endl;
 		}
 	}
 }
-
 void trialDivision(mpz_class &n, vector<mpz_class> &primes){
 	int i = 0;
 
 	while(i < primes.size()){
-//		cout << "testing " << i << " on " << primes[i] << endl;
+		cout << "index: " << i << " testing " << primes[i] << " on " << n << endl;
 			if(n % primes[i] == 0){
 				cout << primes[i] << endl;
 				n = n/primes[i];
@@ -40,18 +40,21 @@ int main(){
 	vector<mpz_class> primes;
 	vector<mpz_class> numberInput;
 	mpz_class n;
-	mpz_class max = 1000000000000;
+	mpz_class max = 10000000000;
 	while(cin >> n){
-		if(mpz_cmp(n.get_mpz_t(), max.get_mpz_t()) > 0){
-			cout << "fail" << endl;
-		}else if(n >= 2){
-			mpz_class tmp = n;
-			getPrimes(primes, tmp);
-			trialDivision(tmp, primes);
-		}
-		cout << endl;
-
-
+		numberInput.push_back(n);
+	}
+for(int i = 0; i < numberInput.size(); ++i){
+	n = numberInput[i];
+	if(mpz_cmp(n.get_mpz_t(), max.get_mpz_t()) > 0){
+		cout << "fail" << endl;
+	}else if(n >= 2){
+		mpz_class tmp = n;
+		getPrimes(primes, tmp);
+		trialDivision(tmp, primes);
+		primes.erase(primes.begin(), primes.end());
+	}
+	cout << endl;
 	}
 	return 0;
 }
