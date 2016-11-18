a5e7c49a72ddc64f1af29fc4df55810b07b4090b
Ziad Giliana <ZiadGiliana@n154-p255.kthopen.kth.se>
2016-11-18 15:07:11 +0100

    isprime added

--- naive.cpp
+++ naive.cpp
@@ -2,36 +2,53 @@
 #include <stdio.h>
 #include <math.h>
 #include <gmpxx.h>
+#include <gmp.h>
 #include <vector>
 #include <time.h>
 using namespace std;
 
+bool isPrime(mpz_class &x){
+	if(mpz_probab_prime_p(x.get_mpz_t(), 5) > 0){
+		return true;
+	}else{
+		return false;
+	}
+}
+/*
 void getPrimes(vector<mpz_class> &primes, mpz_class &n){
 	mpz_class rootN;
 	n.get_mpz_t();
 	rootN.get_mpz_t();
 	rootN = sqrt(n);
-		cout << "root is " << rootN << endl;
+	//	cout << "root is " << rootN << endl;
 	for(mpz_class i = 0; i <= rootN; i++){
 
 		if(mpz_probab_prime_p(i.get_mpz_t(), 50) > 0){
 			primes.push_back(i);
-			cout << "pushed " << i << endl;
+	//		cout << "pushed " << i << endl;
 		}
 	}
 }
+*/
 void trialDivision(mpz_class &n, vector<mpz_class> &primes){
-	int i = 0;
-
-	while(i < primes.size()){
-		cout << "index: " << i << " testing " << primes[i] << " on " << n << endl;
-			if(n % primes[i] == 0){
-				cout << primes[i] << endl;
-				n = n/primes[i];
+	mpz_class rootN;
+	n.get_mpz_t();
+	rootN.get_mpz_t();
+	rootN = sqrt(n);
+	mpz_class i = 2;
+	while(i <= rootN){
+	//	cout << "index: " << " on " << n << endl;
+			if(isPrime(i)){
+				if(n % i == 0){
+					cout << i << endl;
+					n = n/i;
+				}else{
+					i++;
+				}
 			}else{
 				i++;
 			}
-		}
+			}
 		if(n > 1){
 			cout << n << endl;
 		}
@@ -40,19 +57,19 @@ int main(){
 	vector<mpz_class> primes;
 	vector<mpz_class> numberInput;
 	mpz_class n;
-	mpz_class max = 10000000000;
+	mpz_class max("10000000000");
 	while(cin >> n){
 		numberInput.push_back(n);
 	}
-for(int i = 0; i < numberInput.size(); ++i){
-	n = numberInput[i];
-	if(mpz_cmp(n.get_mpz_t(), max.get_mpz_t()) > 0){
-		cout << "fail" << endl;
-	}else if(n >= 2){
-		mpz_class tmp = n;
-		getPrimes(primes, tmp);
-		trialDivision(tmp, primes);
-		primes.erase(primes.begin(), primes.end());
+	for(int i = 0; i < numberInput.size(); ++i){
+		n = numberInput[i];
+		if(mpz_cmp(n.get_mpz_t(), max.get_mpz_t()) > 0){
+			cout << "fail" << endl;
+		}else if(n >= 2){
+			mpz_class tmp = n;
+			//getPrimes(primes, tmp);
+			trialDivision(tmp, primes);
+			primes.erase(primes.begin(), primes.end());
 	}
 	cout << endl;
 	}
