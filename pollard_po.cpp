222683fdf5a6fa7d129ed4dd2a0a44c8dcffd63e
Ziad Giliana <ZiadGiliana@n178-p107.kthopen.kth.se>
2016-11-21 12:20:52 +0100

    tryy

--- pollard_po.cpp
+++ pollard_po.cpp
@@ -6,7 +6,13 @@
 #include <vector>
 #include <time.h>
 using namespace std;
-
+bool isPrime(mpz_class &x){
+	if(mpz_probab_prime_p(x.get_mpz_t(), 5) > 0){
+		return true;
+	}else{
+		return false;
+	}
+}
 bool pollard_rho(mpz_class &n, vector<mpz_class> &primeFactors){
   //Implementera pollard_rho
   mpz_class x = 2;
@@ -14,7 +20,7 @@ bool pollard_rho(mpz_class &n, vector<mpz_class> &primeFactors){
   mpz_class d = 1;
   mpz_class tmp;
   mpz_class toAbs;
-  cout << "inne" << endl;
+  //cout << "inne" << endl;
   while(d == 1){
     x = ((x*x) + 1) % n;
     y = (((((y*y) + 1) % n)*((y*y) + 1) % n) + 1) % n;
@@ -23,41 +29,40 @@ bool pollard_rho(mpz_class &n, vector<mpz_class> &primeFactors){
     mpz_gcd(d.get_mpz_t(), tmp.get_mpz_t(), n.get_mpz_t());
   }
   if(d == n){
-    primeFactors.push_back(d);
     return false;
   }else{
-    primeFactors.push_back(d);
-    n = n/d;
+    if(isPrime(d)){
+      primeFactors.push_back(d);
+      n = n/d;
+    }else{
+      primeFactors.push_back(n/d);
+      n = d;
+
+    }
+    cout << "sending though " << n << endl;
     return true;
   }
 }
-bool isPrime(mpz_class &x){
-	if(mpz_probab_prime_p(x.get_mpz_t(), 5) > 0){
-		return true;
-	}else{
-		return false;
-	}
-}
-void getPrimes(vector<mpz_class> &primes){
-	mpz_class i = 2;
-	while(primes.size() < 100000){
-		if(isPrime(i)){
-			primes.push_back(i);
-	//		cout << "pushed " << i << endl;
-		}
-		i++;
-	}
-}
 
-vector<mpz_class> trialDivision(mpz_class n, vector<mpz_class> &primes){
+void getPrimes(vector<mpz_class> &primes){
+	mpz_class i = 0;
+  mpz_class p;
+  mpz_class tmp = 1;
+  for(i = 1; i < 200000; ++i){
+		mpz_nextprime(p.get_mpz_t(), tmp.get_mpz_t());
+    tmp = p;
+    primes.push_back(p);
+		}
+	}
+
+void trialDivision(mpz_class n, vector<mpz_class> &primes, vector<mpz_class> &primeFactors){
 	int i = 0;
-	vector<mpz_class> v;
 	mpz_class rootN;
 	n.get_mpz_t();
 	rootN.get_mpz_t();
 	rootN = sqrt(n);
 	while(i < primes.size()){
-	//		cout << "testing  " << primes[i] << " on " << n << endl;
+			cout << "testing  " << primes[i] << " on " << n << endl;
 			if(isPrime(n)){
 				break;
 			}
@@ -65,23 +70,17 @@ vector<mpz_class> trialDivision(mpz_class n, vector<mpz_class> &primes){
 				break;
 			}
 			if(mpz_divisible_p(n.get_mpz_t(), primes[i].get_mpz_t()) != 0){
-				v.push_back(primes[i]);
+				primeFactors.push_back(primes[i]);
 				n = n/primes[i];
 			}else{
 				i++;
 			}
 		}
 		if(n > 1 && isPrime(n)){
-			v.push_back(n);
-			return v;
+			primeFactors.push_back(n);
 		}else{
-      cout << "running pollard: " << n << endl;
-      while(pollard_rho(n, v)){
-        cout << "found n: " << n << endl;
-        //Well do nothing
+        primeFactors.clear();
       }
-      return v;
-    }
 }
 int main(){
 	vector<mpz_class> primes;
@@ -89,20 +88,26 @@ int main(){
 	getPrimes(primes);
 	mpz_class n;
 	while(cin >> n){
+    primeFactors.clear();
 		n.get_mpz_t();
 		if(isPrime(n)){ //Is prime just print
 			cout << n << endl;
 		}else{ //Try to factor
-      bool morePrimes = true;
-			primeFactors = trialDivision(n, primes);
-      if(isPrime(primeFactors[primeFactors.size()-1]) && primeFactors[primeFactors.size()-1] > 1){
+      pollard_rho(n, primeFactors);
+    //  cout <<"found: "<< n << endl;
+      trialDivision(n, primes, primeFactors);
+
+
+
+
+      if(!primeFactors.empty()){
 			for(int i = 0; i < primeFactors.size(); ++i){
 				cout << primeFactors[i] << endl;
 			}
 		}else{
 			cout << "fail" << endl;
 		}
-      //Forsätt med sista talet från trialDivsion och kolla om det är prime
+     //Forsätt med sista talet från trialDivsion och kolla om det är prime
       //Om inte så ska vi köra pollard rho på det
 
 		}
